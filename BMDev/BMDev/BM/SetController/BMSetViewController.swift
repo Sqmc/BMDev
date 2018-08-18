0a25bc92c6642f0962df38fa518917a8342b25ab
Max <baimakejigognsi@sina.cn>
2018-08-18 16:04:09 +0800

    mm

---
+++ BMDev/BMDev/BM/SetController/BMSetViewController.swift
@@ -0,0 +1,20 @@
+//
+//  BMSetViewController.swift
+//  BMDev
+//
+//  Created by Max on 2018/8/18.
+//  Copyright © 2018年 Max. All rights reserved.
+//
+
+import UIKit
+
+class BMSetViewController: BaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.navigationItem.title="设置"
+    }
+
+
+}
