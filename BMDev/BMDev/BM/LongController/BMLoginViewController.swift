8d53ea8ecdc7cf6cbc6b82dfed1ee47f56f3dea2
Max <baimakejigongsi@sina.cn>
2018-08-18 14:25:23 +0800

    添加登录

---
+++ BMDev/BMDev/BM/LongController/BMLoginViewController.swift
@@ -0,0 +1,20 @@
+//
+//  BMLoginViewController.swift
+//  BMDev
+//
+//  Created by Max on 2018/8/18.
+//  Copyright © 2018年 Max. All rights reserved.
+//
+
+import UIKit
+
+class BMLoginViewController: BaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.navigationItem.title="登录"
+
+    }
+
+}
